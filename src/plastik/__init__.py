6e4fb65c2fa74f2b9f64ab6a560de16d017423c7
engeir <engeir@pm.me>
2024-03-20 10:53:18 +0100

    feat(plot): add percentiles plotting function

--- src/plastik/__init__.py
+++ src/plastik/__init__.py
@@ -1,9 +1,12 @@
+"""Library for creating beautiful and insightful visualizations."""
+
 from importlib_metadata import version
 
 from plastik import colors
 from plastik.axes import *  # noqa:F401,F403
 from plastik.legends import *  # noqa:F401,F403
+from plastik.percentiles import percentiles
 from plastik.ridge import *  # noqa:F401,F403
 
 __version__ = version(__package__)
-__all__ = ["colors"]
+__all__ = ["colors", "percentiles"]
