d4236b72adae662b30e015c21b3ed3011b4d960a
engeir <engeir@pm.me>
2024-12-05 17:25:47 +0100

    feat(airport): add airport plot class

--- src/plastik/__init__.py
+++ src/plastik/__init__.py
@@ -9,5 +9,17 @@ from plastik.legends import *  # noqa:F401,F403
 from plastik.percentiles import percentiles
 from plastik.ridge import *  # noqa:F401,F403
 
+try:
+    from plastik.airport import Airport, airport
+except ImportError:
+
+    def need_extra(*_, **k):
+        raise ImportError(
+            "Please install with extra dependencies: pip install plastik[extra]"
+        )
+
+    Airport = need_extra  # type: ignore[misc,assignment]
+    airport = need_extra  # type: ignore[assignment]
+
 __version__ = version(__package__)
-__all__ = ["colors", "lines", "percentiles"]
+__all__ = ["Airport", "airport", "colors", "lines", "percentiles"]
